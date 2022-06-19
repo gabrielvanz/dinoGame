d15354a925f99fc3e5a3276b8804cd84eb084dc9
gabrielvanz <90943303+gabrielvanz@users.noreply.github.com>
2022-06-19 14:04:13 -0300

    Background e Mobs

--- dino.py
+++ dino.py
@@ -1,4 +1,5 @@
-from re import X
+import imp
+from funcoes import colisao,morreu
 import pygame
 import random
 import time
@@ -8,35 +9,45 @@ altura = 300
 
 tamanho = (largura, altura)
 pygameDisplay = pygame.display
-pygameDisplay.set_caption("Star Wars")
+pygameDisplay.set_caption("Dino Game")
 gameDisplay = pygame.display.set_mode(tamanho)
-#gameIcon = pygame.image.load("assets/TrupperIco.ico")
-#pygameDisplay.set_icon(gameIcon)
+gameIcon = pygame.image.load("imagens/icone.ico")
+pygameDisplay.set_icon(gameIcon)
 
+#Cores
 branco = (255, 255, 255)
-
-
+preto = (0, 0, 0)
 
 clock = pygame.time.Clock()
 gameEvents = pygame.event
 
+def dead(pontos):
+    #gameDisplay.blit(bg_destroy, (0, 0))
+    pygame.mixer.music.stop()
+    #pygame.mixer.Sound.play(explosaoSound)
+    fonte = pygame.font.Font("freesansbold.ttf", 50)
+    fonteContinue = pygame.font.Font("freesansbold.ttf", 25)
+    texto = fonte.render("Você Perdeu com "+str(pontos) +
+                         " pontos!", True, preto)
+    textoContinue = fonteContinue.render(
+        "Press enter to continue...", True, branco)
+    gameDisplay.blit(textoContinue, (50, 200))
+    gameDisplay.blit(texto, (50, 100))
+    pygameDisplay.update()
+
 def jogo():
     x = 0
-    fps = 0
+    y = 230
+    fpsDino = 0
+    fpsPassaro = 0
     i = 0
-    posicaoX = 0
+    posicaoX = 800
     posicaoY = random.randrange(0, altura)
     
-    velocidade = 1
+    velocidade = 5
     pontos = 0
-    lista = []
-
-    for teste in range(0,largura,70):
-        lista.append(teste)
 
     tamanhoPadrao = (70,70)
-
-    print (lista)
 
     dino1 = pygame.image.load("imagens/dino1.png")
     dino2 = pygame.image.load("imagens/dino2.png")
@@ -47,24 +58,47 @@ def jogo():
 
     dino = [dino2,dino1,dino2,dino3]
 
+    cacto = pygame.image.load("imagens/cacto.png")
+    cacto = pygame.transform.scale(cacto, (60,60))
+
+    passaro1 = pygame.image.load("imagens/passaro1.png")
+    passaro2 = pygame.image.load("imagens/passaro2.png")
+    passaro1 = pygame.transform.scale(passaro1, (70,70))
+    passaro2 = pygame.transform.scale(passaro2, (70,70))
+
+    nuvem = pygame.image.load("imagens/nuvem.png")
+    nuvem = pygame.transform.scale(nuvem, tamanhoPadrao)
+
     chao = pygame.image.load("imagens/chao.png")
-    chao = pygame.transform.scale(chao, tamanhoPadrao)
     
-    """
-    pygame.mixer.music.load("assets/trilha.mp3")
+    """pygame.mixer.music.load("assets/trilha.mp3")
     pygame.mixer.music.play(-1)
     pygame.mixer.music.set_volume(0.1)
     """
     
+    nuvemAX = 800
+    nuvemAY = random.randint(0,130)
+    nuvemBX = 900
+    nuvemBY = random.randint(0,130)
+    nuvemCX = 1000
+    nuvemCY = random.randint(0,130)
+
     gravidade = 0
+    dinoX = 150
     dinoY = 206
+    alturaDino = 70
+    larguraDino = 70
 
-    alturaDino = 32
-    larguraDino = 32
+    cactoX = largura
+    varCactoX = 0
+    
+    passaroX = largura+cactoX
+    varPassaroX = 100
+    passaroY = random.randint(180,200)
+    velocidadePassaro = random.randint(5,8)
 
-    alturaMissel = 52
-    larguraMissel = 150
-    dificuldade = 19
+    pulo = False
+    dificuldade = 35
 
     while True:
         # aqui é lido os eventos da tela
@@ -73,71 +107,118 @@ def jogo():
                 pygame.quit()
                 quit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_KP_ENTER:
+                if event.key == pygame.K_r:
                     jogo()
-                if event.key == pygame.K_SPACE:
-                    gravidade = -15
+                if pulo == True:
+                    if event.key == pygame.K_SPACE or event.key == pygame.K_w or event.key == pygame.K_UP:
+                        gravidade = -10
                     
-                    
-        gravidade = gravidade + 1
-        x -=1
-        if x<-1:
-            x=largura
-        dinoY+=gravidade
-
-        if dinoY > 206: dinoY = 206
-
         gameDisplay.fill(branco)    
-        
-        gameDisplay.blit(chao,(lista[0]+x,230))
-        gameDisplay.blit(chao,(lista[1]+x,230))
-        gameDisplay.blit(chao,(lista[2]+x,230))
-        gameDisplay.blit(chao,(lista[3]+x,230))
-        gameDisplay.blit(chao,(lista[4]+x,230))
-        gameDisplay.blit(chao,(lista[5]+x,230))
-        gameDisplay.blit(chao,(lista[6]+x,230))
-        gameDisplay.blit(chao,(lista[7]+x,230))
-        gameDisplay.blit(chao,(lista[8]+x,230))
-        gameDisplay.blit(chao,(lista[9]+x,230))
-        gameDisplay.blit(chao,(lista[10]+x,230))
-        gameDisplay.blit(chao,(lista[11]+x,230))
 
-        
-            
-        
+        pontos +=1
 
-        if fps <15:
-            fps+=1
-            gameDisplay.blit(dino[0], (100, dinoY))
-        elif fps<=30:
-            fps+=1
-            gameDisplay.blit(dino[1], (100, dinoY))
-        elif fps<=45:
-            fps+=1
-            gameDisplay.blit(dino[2], (100, dinoY))
-        elif fps<=60:
-            fps+=1
-            gameDisplay.blit(dino[3], (100, dinoY))
+        #Chão
+        gameDisplay.blit(chao,(x,230))
+        gameDisplay.blit(chao,(largura+x,230))
+        
+        if (x==-largura):
+            gameDisplay.blit(chao,(largura+x,230))
+            x=0
+        x-=velocidade
+
+        #Nuvens
+        nuvemAX-=velocidade
+        nuvemBX-=velocidade
+        nuvemCX-=velocidade
+        gameDisplay.blit(nuvem,(nuvemAX,nuvemAY))
+        gameDisplay.blit(nuvem,(nuvemBX,nuvemBY))
+        gameDisplay.blit(nuvem,(nuvemCX,nuvemCY))
+        if nuvemAX<-largura:
+            nuvemAX=largura+random.randint(0,70)
+            nuvemAY=random.randint(0,120)
+        if nuvemBX<-largura:
+            nuvemBX=largura+random.randint(70,140)
+            nuvemBY=random.randint(0,120)
+        if nuvemCX<-largura:
+            nuvemCX=largura+random.randint(140,210)
+            nuvemCY=random.randint(0,120)
+
+        #Dino
+        if dinoY == 206:
+            pulo=True
         else:
-            fps=0
-            gameDisplay.blit(dino[0], (100, dinoY))
-            
+            pulo=False
 
+        gravidade = gravidade + 0.4
+        dinoY+=gravidade
+        if dinoY > 206:
+            dinoY = 206
+        if pulo==True:
+            if fpsDino <5:
+                fpsDino+=1.5
+                gameDisplay.blit(dino[0], (dinoX, dinoY))
+            elif fpsDino<=10:
+                fpsDino+=1.5
+                gameDisplay.blit(dino[1], (dinoX, dinoY))
+            elif fpsDino<=15:
+                fpsDino+=1.5
+                gameDisplay.blit(dino[2], (dinoX, dinoY))
+            elif fpsDino<=20:
+                fpsDino+=1.5
+                gameDisplay.blit(dino[3], (dinoX, dinoY))
+            else:
+                fpsDino=0
+                gameDisplay.blit(dino[0], (dinoX, dinoY))
+        else:
+            gameDisplay.blit(dino[0], (dinoX, dinoY))
 
-
-
-
-
-
-
-
-
-
+        #Cacto
+        cactoPosX = cactoX + varCactoX
+        cactoX-=velocidade
+        gameDisplay.blit(cacto, (cactoPosX,204))
+        if cactoX<-largura:
+            varCactoX = random.randint(50,250)
+            cactoX = largura+varCactoX
         
-       
+        #Pássaro
+        passaroPosX = passaroX + varPassaroX
+        passaroX-=velocidadePassaro
+        fpsPassaro+=1
+        if fpsPassaro < 10:
+            gameDisplay.blit(passaro1, (passaroPosX, passaroY))
+        elif fpsPassaro < 20:
+            gameDisplay.blit(passaro2, (passaroPosX, passaroY))
+        else:
+            fpsPassaro = 0
+            gameDisplay.blit(passaro1, (passaroPosX,passaroY))
+        if passaroX<-largura:
+            passaroX = largura+varPassaroX
+            passaroY = random.randint(100,200)
+            varPassaroX = random.randint(250,400)
+            velocidadePassaro = random.randint(5,10)
+            
+        # Análise de Colisão
+        pixelsYDino = list(
+            range(int(dinoY), int(dinoY) + alturaDino+1))
+        pixelsXDino = list(
+            range(dinoX, dinoX + larguraDino+1))
+        
+        pixelsYCacto = list(
+            range(204, 204 + 60+1))
+        pixelsXCacto = list(
+            range(cactoPosX, cactoPosX + 60+1))
+
+        pixelsYPassaro = list(
+            range(passaroY, passaroY + 71))
+        pixelsXPassaro = list(
+            range(passaroPosX, passaroPosX + 71))
+
+        # Analise de colisão com cacto
+        colisao(pixelsYCacto,pixelsYDino,pixelsXCacto,pixelsXDino,dificuldade)
+        # Analise de colisão com ave
+        colisao(pixelsYPassaro,pixelsYDino,pixelsXPassaro,pixelsXDino,dificuldade)
 
         pygameDisplay.update()
         clock.tick(60)
-
 
 jogo()
